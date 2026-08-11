b68a7f02b7bdda9298219d21277e431a2a5c891a
abusufyanm-png <abusufyanm@gmail.com>
2026-08-11 13:12:02 +0100

    Track missing wallet migration

---
+++ Data/Migrations/20260706041832_wallet.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CTSHIPDashboard.Migrations
+{
+    /// <inheritdoc />
+    public partial class wallet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                IF COL_LENGTH('Providers', 'WalletSource') IS NULL
+                BEGIN
+                    ALTER TABLE [Providers] ADD [WalletSource] nvarchar(max) NULL;
+                END
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                IF COL_LENGTH('Providers', 'WalletSource') IS NOT NULL
+                BEGIN
+                    ALTER TABLE [Providers] DROP COLUMN [WalletSource];
+                END
+                """);
+        }
+    }
+}
